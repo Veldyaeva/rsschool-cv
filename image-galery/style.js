fea4dad15fab5743c272b40680e5a4f45254af3c
Veldyaeva <pechalno777@gmail.com>
2023-09-30 11:05:21 +0300

    feat:add search request

--- image-galery/style.js
+++ image-galery/style.js
@@ -1,16 +1,21 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const I = document.querySelector('input');
+//const searchRequest = I.value;
+//const url = `https://api.unsplash.com/search/photos?query='${searchRequest}'&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
 const random_img = 'https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 const imgSrc = document.querySelector('.imgs'); 
 //const img = document.querySelectorAll('.img');
 const galleryContainer = document.querySelector('.imgs');
+let searchRequest = 'dog';
 
-async function getData() {
+async function getData(searchRequest) {
+    let url = `https://api.unsplash.com/search/photos?query='${searchRequest}'&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
+    console.log(searchRequest, url);
     const res = await fetch(url);
     const data = await res.json();
     console.log(data.results[0].urls.small);
     
     data.results.forEach(element => {
-        console.log(element.urls.small);
+        //console.log(element.urls.small);
         let imgUrl = element.urls.small;
         showData(imgUrl);
     });
@@ -28,4 +33,7 @@ async function showData(url)
     imgCont.append(img);
     //galleryContainer.append(img);
 }
-  getData();
+  getData(searchRequest);
+I.addEventListener('keydown', function(e) {if (e.keyCode ===13)
+{getData(I.value)}
+});
